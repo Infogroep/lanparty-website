8bbcfc191869f79dedfd06fbad9ba45f6fbbd9de
Roeland Matthijssens <fulgens.ailurus@gmail.com>
2012-09-30 23:34:19 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lanparty::Application.config.secret_token = 'f8e51c61853caa326bb1e8dea588d3bd5d0909c0ef75a043c604bb765d1195fd7b09ca36b5452437bd342992d93d62cea2977ffe232c496eaf29431233e8c902'
