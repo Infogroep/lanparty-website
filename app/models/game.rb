d7b33a28fc994045951465b9c256c5dba2b47172
Roeland Matthijssens <fulgens.ailurus@gmail.com>
2013-01-09 22:21:23 +0100

    image uploading done
    featured compo's game's images will be displayed on the front page
    carousel

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,9 +1,11 @@
 class Game < ActiveRecord::Base
-	attr_accessible :download_location, :info, :name
+	attr_accessible :download_location, :info, :name, :image
 
 	validates_presence_of :download_location
 	validates_uniqueness_of :download_location
 	validates_presence_of :name
 	validates_uniqueness_of :name
+
+	mount_uploader :image, ImageUploader
 
 end
