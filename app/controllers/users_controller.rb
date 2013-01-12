b7994620f37a12f153283c411dc2415ce4cd9fdd
Ruben Tytgat <ruben.tytgat@gmail.com>
2013-01-12 02:33:15 +0100

    Pretty cool user edit access control

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 	before_filter :login_required, :except => [:new, :create]
+  before_filter(:only => [:edit]) { params[:id] == self.current_user.id.to_s || access_required(:user_editing) }
 
 	def index
 		@users = User.all
