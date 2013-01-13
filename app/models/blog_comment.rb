35a232f388ab14ef5c24b46f614c333c8335e70a
Ruben Tytgat <ruben.tytgat@gmail.com>
2013-01-13 15:18:45 +0100

    Using simple form now

--- app/models/blog_comment.rb
+++ app/models/blog_comment.rb
@@ -2,4 +2,8 @@ class BlogComment < ActiveRecord::Base
   belongs_to :blog_post
   belongs_to :user
   attr_accessible :content, :blog_post_id, :user_id
+
+  validates_presence_of :content
+  validates_presence_of :blog_post
+  validates_presence_of :user
 end
--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -2,4 +2,8 @@ class BlogPost < ActiveRecord::Base
   belongs_to :user
   has_many :blog_comments
   attr_accessible :content, :title, :user_id
+
+  validates_presence_of :content
+  validates_presence_of :title
+  validates_presence_of :user
 end
