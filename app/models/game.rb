5259aab23e180f64229e8b48e84b27e9c68aa0c0
Ruben Tytgat <ruben.tytgat@gmail.com>
2013-01-23 23:29:08 +0100

    No presence validation for download location

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,8 @@
 class Game < ActiveRecord::Base
 	attr_accessible :download_location, :info, :name, :image
 
-	validates_presence_of :download_location
-	validates_uniqueness_of :download_location
+#	validates_presence_of :download_location
+#	validates_uniqueness_of :download_location
 	validates_presence_of :name
 	validates_uniqueness_of :name
 
