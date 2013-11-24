5bc142bc6f6ec238bed3a8bda84232a97cdc6ff3
angryzor <ruben_tytgat@hotmail.com>
2013-11-24 21:11:37 +0100

    Converted some more pages to HAML

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
-	def avatar_url user
+	def avatar_url user, size = 48
 		gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-		"http://gravatar.com/avatar/#{gravatar_id}.png?s=48&"
+		"http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
 	end
 end
